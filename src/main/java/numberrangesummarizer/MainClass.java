af55eb701bf92ca1b2fe205cddc721975d6e6140
Stéphan van Biljon <stephanvanb@gmail.com>
2023-05-16 01:34:13 +0200

    Changed main method output.

--- src/main/java/numberrangesummarizer/MainClass.java
+++ src/main/java/numberrangesummarizer/MainClass.java
@@ -5,11 +5,13 @@ import numberrangesummarizer.NumberRangeSummarizerImpl;
 
 public class MainClass {
     public static void main(String[] args) {
+        String input = "1,3,6,7,8,12,13,14,15,21,22,23,24,31";
         NumberRangeSummarizerImpl summarizer = new NumberRangeSummarizerImpl();
-        Collection<Integer> numbers = summarizer.collect("1,3,6,7,8,12,13,14,15,21,22,23,24,31");
+        Collection<Integer> numbers = summarizer.collect(input);
 
         String result = summarizer.summarizeCollection(numbers);
 
-        System.out.println(result);
+        System.out.println("Input String: " + input);
+        System.out.println("Output String: " + result);
     }
 }
